d932adae0600445ab0f22987b2f7e19cecf46465
github_anaeem59 <anaeem59@gmail.com>
2021-04-18 17:09:46 +0530

    Day 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
+import Banner from './Banner';
+import Nav from './Nav';
 import requests from './requests';
 import Row from './Row';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hey</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} />
+      <Nav />
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
@@ -15,5 +24,3 @@ function App() {
 export default App;
 
 
-// 
-// https://api.themoviedb.org/3/movie/550?api_key=c0845bf0572b5c4075ae24c14f0e5c32
